9c1be27ecb51e7a1dc7553392115e669021f8a22
tfamu <tuanphamdummy1@gmail.com>
2025-08-09 22:17:27 +0900

    implement Layout and react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,34 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import NewUsers from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import { GlobalStyles } from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Heading from "./ui/Heading";
-import Input from "./ui/Input";
-import Row from "./ui/Row";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Row type="vertical">
-        <Row type="horizontal">
-          <Heading as="h1">The Wild Oasis</Heading>
-          <div>
-            <Heading as="h2">Check in and out</Heading>
-            <Button
-              variation="primary"
-              size="medium"
-              onClick={() => alert("check in")}
-            >
-              Check in
-            </Button>
-            <Button
-              variation="secondary"
-              size="small"
-              onClick={() => alert("check out")}
-            >
-              Check out
-            </Button>
-          </div>
-        </Row>
-      </Row>
-      <Row type="vertical">
-        <Heading as="h3">Form</Heading>
-        <form>
-          <Input type="number" placeholder="Number of Guests" />
-          <Input type="number" placeholder="Number of Guests" />
-        </form>
-      </Row>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<NewUsers />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
