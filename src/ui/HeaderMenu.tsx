6bf5a7fd525900f23b94f17cd797f096c9e486f1
tfamu <tuanphamdummy1@gmail.com>
2025-09-07 15:30:19 +0900

    impl darkmode

--- src/ui/HeaderMenu.tsx
+++ src/ui/HeaderMenu.tsx
@@ -3,26 +3,30 @@ import ButtonIcon from "./ButtonIcon"
 import Logout from "../features/authentication/Logout"
 import { useNavigate } from "react-router"
 import { HiOutlineUser } from "react-icons/hi2"
+import DarkModeToggle from "./DarkModeToggle";
 
 const StyledHeaderMenu = styled.ul`
-    display: flex;
-    gap: 0.4rem;
-`
+  display: flex;
+  gap: 0.4rem;
+`;
 
 const HeaderMenu = () => {
-    const navigate = useNavigate()
-    return (
-        <StyledHeaderMenu>
-            <li>
-                <ButtonIcon onClick={() => navigate("/account")} >
-                    <HiOutlineUser />
-                </ButtonIcon>
-            </li>
-            <li>
-                <Logout />
-            </li>
-        </StyledHeaderMenu>
-    )
-}
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+};
 
 export default HeaderMenu
